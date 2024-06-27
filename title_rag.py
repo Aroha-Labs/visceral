e5ac23a3462bcb1410e64694c43aafbecdd17b6f
Prathamesh Satpute <prathamesh1309@gmail.com>
2024-06-27 13:55:57 +0530

    added generated titles

--- title_rag.py
+++ title_rag.py
@@ -24,16 +24,23 @@ base_prompt = """{}
 Based on the above game data, for this game description, age rating and tags generate 5 game titles. Return the titles only and no other text.
 {}
 """
-
+generated_titles = []
 for index, row in test_df.iterrows():
     age_rating = row['Age Rating']
     filtered_df = train_df[train_df['Age Rating'] == age_rating]
     retrieved_text = filtered_df.to_string()
     input_text = row[['Description', 'Age Rating', 'Tags']].to_string(index=False, header=False)
     prompt = base_prompt.format(retrieved_text, input_text)
+    generated_title = rag_flow(prompt)
+    generated_titles.append(generated_title)
 
     print("Actual Title:", row['Game '])
-    print("Generated Titles:\n", rag_flow(prompt))
+    print("Generated Titles:\n", generated_title)
 
+test_df['Generated Title'] = generated_titles
+
+
+output_csv = 'rag_generated_titles.csv'
+test_df.to_csv(output_csv, index=False)
 
 
